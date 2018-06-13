88e46dd59f259ed011d863bd2edd0729193b6e7f
mark@zijdemans.com <mark@zijdemans.com>
2018-06-13 14:02:23 +0200

    grub menu,

--- create_iso.sh
+++ create_iso.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 mkdir -p iso/boot/grub              # create the folder structure
 cp stage2_eltorito iso/boot/grub/   # copy the bootloader
 cp kernel.elf iso/boot/             # copy the kernel
