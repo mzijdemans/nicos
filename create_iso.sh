ee76d374ca9420f2a058d8a0aa2607b102f02dbc
mark@zijdemans.com <mark@zijdemans.com>
2018-06-13 14:06:05 +0200

    bochs config file

--- create_iso.sh
+++ create_iso.sh
@@ -2,3 +2,22 @@
 mkdir -p iso/boot/grub              # create the folder structure
 cp stage2_eltorito iso/boot/grub/   # copy the bootloader
 cp kernel.elf iso/boot/             # copy the kernel
+
+#    iso
+#    |-- boot
+#      |-- grub
+#      | |-- menu.lst
+#      | |-- stage2_eltorito
+#      |-- kernel.elf
+
+# generate the ISO
+genisoimage -R                              \
+            -b boot/grub/stage2_eltorito    \
+            -no-emul-boot                   \
+            -boot-load-size 4               \
+            -A os                           \
+            -input-charset utf8             \
+            -quiet                          \
+            -boot-info-table                \
+            -o os.iso                       \
+            iso
